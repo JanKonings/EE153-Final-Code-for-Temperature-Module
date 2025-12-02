8690d761e28ecf9d0060840bdaef06cdc74b848e
JanKonings <jankonings12@gmail.com>
2025-12-02 11:42:08 -0500

    addedm some to main

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/spi_master.h"
+#include "esp_err.h"
+
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+#include "driver/gpio.h"
+#include <math.h> 
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "mqtt_client.h"
+#include "minimal_wifi.h"
+
+
+void app_main(void)
+{
+    
+}
