96bcf79f2b611d47de3c7c09c633d5bae708065d
JanKonings <jankonings12@gmail.com>
2025-12-02 11:49:44 -0500

    added mqtt code

--- src/main.c
+++ src/main.c
@@ -15,8 +15,38 @@
 #include "mqtt_client.h"
 #include "minimal_wifi.h"
 
+// MQTTT STUFF
+#define WIFI_SSID      "tufts_eecs"
+#define WIFI_PASS      "foundedin1883"
+
+#define BROKER_URI "mqtt://bell-mqtt.eecs.tufts.edu"
 
 void app_main(void)
 {
-    
+    // Enable Flash (aka non-volatile storage, NVS)
+    esp_err_t ret2 = nvs_flash_init();
+    if (ret2 == ESP_ERR_NVS_NO_FREE_PAGES || ret2 == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret2 = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret2);
+
+    // Normally we'd need to initialize the event loop and the network stack,
+    // but both of these will be done when we connect to WiFi.
+    printf("Connecting to WiFi...");
+    wifi_connect(WIFI_SSID, WIFI_PASS);
+
+    // Initialize the MQTT client
+    // Read the documentation for more information on what you can configure:
+    // https://docs.espressif.com/projects/esp-idf/en/latest/esp32c3/api-reference/protocols/mqtt.html
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = BROKER_URI,
+    };
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(client);
+
+
+    //publish
+    esp_mqtt_client_publish(client, "jkonin01/iteration1/thermistor_temp", , 0, 0, 0);
+
 }
